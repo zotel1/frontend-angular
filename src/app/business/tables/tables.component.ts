ba33943af76b3107ebd3c51fe5cc3f0858a00417
zotel1 <zotelsigel@gmail.com>
2025-01-04 14:23:40 -0300

    add, components on dashboard

---
+++ src/app/business/tables/tables.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tables',
+  imports: [],
+  templateUrl: './tables.component.html',
+  styleUrl: './tables.component.css'
+})
+export class TablesComponent {
+
+}
