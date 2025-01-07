499accbf05a6673bf468b235d0b5a768ef58e275
zotel1 <zotelsigel@gmail.com>
2025-01-06 23:53:09 -0300

    update, metds auth

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,51 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { response } from 'express';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+
+    private LOGIN_URL = 'http://loccalhost:3000/api/v1/auth/login';
+    private tokenKey = 'authToken';
+
+  constructor(private httpClient: HttpClient, private router: Router) { }
+
+  login(user: string, password: string): Observable<any>{
+    return this.httpClient.post<any>(this.LOGIN_URL, {user, password}).pipe(
+        tap(response => {
+            if(response.token){
+                console.log(response.token)
+            }
+        })
+    )
+  }
+
+  private setToken(token: string): void {
+    localStorage.setItem(this.tokenKey, token);
+  }
+
+  private getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+  
+  isAuthenticated(): boolean {
+    const token = this.getToken();
+    if(!token){
+        return false;
+    }
+
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    const exp = payload.exp * 1000;
+    return Date.now() < exp;
+  }
+
+  logout(): void{
+    localStorage.removeItem(this.tokenKey);
+    this.router.navigate(['/login']);
+  }
 }
