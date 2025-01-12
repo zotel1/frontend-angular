75ca43080160f7a4045ea0c50bede58fd647993f
zotel1 <zotelsigel@gmail.com>
2025-01-12 12:33:50 -0300

    update tables.component.ts

--- src/app/business/tables/tables.component.ts
+++ src/app/business/tables/tables.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../core/services/api.service';
 
 @Component({
   selector: 'app-tables',
@@ -6,6 +7,37 @@ import { Component } from '@angular/core';
   templateUrl: './tables.component.html',
   styleUrl: './tables.component.css'
 })
-export default class TablesComponent {
+export default class TablesComponent implements OnInit{
+    plants: any[] = [];
+    countries: any[] = [];
+
+    constructor(private apiService: ApiService) {}
+
+    ngOnInit(): void {
+        this.loadPlants();
+        this.loadCountries();
+    }
+
+    // Cargamos las plantas
+    loadPlants(): void {
+        this.apiService.getPlants().subscribe({
+            next: (data) => {
+                console.log('Plantas', data);
+                this.plants = data;
+            },
+            error: (err) => console.error('Error al cargar plantas', err)
+        });
+    }
+
+    // Cargamos paises
+    loadCountries(): void {
+        this.apiService.getCountries().subscribe({
+            next: (data) => {
+                console.log('Países:', data);
+                this.countries = data;
+            },
+            error: (err) => console.error('Error al cargar países.', err)
+        });
+    }
 
 }
