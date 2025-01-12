f518924ca0161f204a330cab7689a060e09a16ac
zotel1 <zotelsigel@gmail.com>
2025-01-12 13:31:33 -0300

    update tables layout

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { FooterComponent } from '../footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from '../../../core/services/api.service';
 
 @Component({
   selector: 'app-layout',
@@ -11,6 +12,36 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
-export default class LayoutComponent {
+export default class LayoutComponent implements OnInit {
+    plants: any[] = [];
+    countries: any[] = [];
 
+    constructor(private apiService: ApiService) { }
+
+    ngOnInit(): void {
+        this.loadPlants();
+        this.loadCountries();
+    }
+
+    // Cargar plantas
+    loadPlants(): void {
+        this.apiService.getPlants().subscribe({
+            next: (data) => {
+                console.log('Plantas:', data);
+                this.plants = data;
+            },
+            error: (err) => console.error('Error al cargar plantas', err)
+        });
+    }
+
+    // Cargar países
+    loadCountries(): void {
+        this.apiService.getCountries().subscribe({
+            next: (data) => {
+                console.log('Países:', data);
+                this.countries = data;
+            },
+            error: (err) => console.error('Error al cargar países', err)
+        });
+    }
 }
