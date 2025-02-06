78a20f4545c173be69147310df6f9e344379ec2b
zotel1 <zotelsigel@gmail.com>
2025-02-05 21:35:04 -0300

    creamos un nuevo metodo en nuestro dashboard.ts  llamado totalSummary para traer la suma de todos los datos en todas las plantas

--- src/app/business/dashboard/dashboard.component.ts
+++ src/app/business/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../core/services/api/api.service';
 import { AuthService } from '../../core/services/auth/auth.service';
 import { PlantDetailComponent } from '../plants-detail/plant-detail.component';
-import { Plant } from '../../core/models/model';
+import { Plant, Summary } from '../../core/models/model';
 import { RouterOutlet } from '@angular/router';
 import { PlantsComponent } from '../plant/plants.component';
 
@@ -17,6 +17,7 @@ import { PlantsComponent } from '../plant/plants.component';
 export class DashboardComponent implements OnInit {
     userName: string | null = null; // Nombre de usuario obtenido del token
     summary: any = null; // Datos del resumen de alertas
+    totalSummary: Summary | null = null; // Datos del resumen total
     selectedPlant: Plant | null = null; // Planta seleccionada desde la tabla
     selectedPlantFlag: string | null = null; // Bandera asociada a la planta seleccionada
 
@@ -24,6 +25,7 @@ export class DashboardComponent implements OnInit {
 
     ngOnInit(): void {
         this.fetchSummary(); // Obtener datos del resumen
+        this.fetchTotalSummary();
         this.userName = this.authService.getUsernameFromToken(); // Obtener nombre de usuario
     }
 
@@ -42,6 +44,23 @@ export class DashboardComponent implements OnInit {
             },
             error: (err) => {
                 console.error('Error al obtener el resumen:', err);
+            }
+        });
+    }
+
+    fetchTotalSummary(): void {
+        this.apiService.getPlants().subscribe({
+            next: (plants: Plant[]) => {
+                this.totalSummary = {
+                    cantidadLecturas: plants.reduce((sum, plant) => sum + (plant.cantidadLecturas || 0), 0),
+                    alertasMedias: plants.reduce((sum, plant) => sum + (plant.alertasMedias || 0), 0),
+                    alertasRojas: plants.reduce((sum, plant) => sum + (plant.alertasRojas || 0), 0),
+                    sensoresInactivos: plants.reduce((sum, plant) => sum + (plant.sensoresInactivos || 0), 0),
+                };
+                console.log('Resumen total calculado:', this.totalSummary);
+            },
+            error: (err) => {
+                console.error('Error al obtener el resumen total:', err);
             }
         });
     }
